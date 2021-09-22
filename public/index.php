77a55821b35d73951f534c271304b93660b91120
Swift <dxdejong@gmail.com>
2021-09-22 10:03:07 +0200

    work on homepage

--- public/index.php
+++ public/index.php
@@ -3,12 +3,15 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+session_start();
 require_once __DIR__.'/../vendor/autoload.php';
 
 use App\Helpers\Route;
 
-function handleRequest($method, $request) {
-    Route::handle($method, $request);
-}
+// function handleRequest($method, $request) {
+//     Route::handle($method, $request);
+// }
 
-handleRequest($_SERVER['REQUEST_METHOD'], $_REQUEST);
+Route::handle($_SERVER['REQUEST_METHOD'], $_REQUEST);
+
+// handleRequest($_SERVER['REQUEST_METHOD'], $_REQUEST);
--- resources/views/welcome.php
+++ resources/views/welcome.php
@@ -13,6 +13,22 @@
         <div class="logo">MDMA</div>
         <h2>Welcome, start <span class="text-secondary">listening</span> right away.</h2>
         <a href="" class="btn btn-primary btn-xl">Start now</a>
+        <a href="#info" class="scroll-down">
+            <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                width="451.847px" height="451.847px" viewBox="0 0 451.847 451.847" style="enable-background:new 0 0 451.847 451.847;"
+                xml:space="preserve">
+                <g>
+                    <path d="M225.923,354.706c-8.098,0-16.195-3.092-22.369-9.263L9.27,151.157c-12.359-12.359-12.359-32.397,0-44.751
+                        c12.354-12.354,32.388-12.354,44.748,0l171.905,171.915l171.906-171.909c12.359-12.354,32.391-12.354,44.744,0
+                        c12.365,12.354,12.365,32.392,0,44.751L248.292,345.449C242.115,351.621,234.018,354.706,225.923,354.706z"/>
+                </g>
+            </svg>
+        </a>
+        <svg class="waves" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#160220" fill-opacity="1" d="M0,160L60,154.7C120,149,240,139,360,144C480,149,600,171,720,186.7C840,203,960,213,1080,218.7C1200,224,1320,224,1380,224L1440,224L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path></svg>
     </div>    
+
+    <section id="info">
+        <img src="img/image_processing20210619-27954-l9gcd3.png">
+    </section>
 </body>
 </html>
