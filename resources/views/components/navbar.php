89e045f62f95bb6aefe9350e115c3ad5e4a37afd
Swift <dxdejong@gmail.com>
2021-10-04 13:55:17 +0200

    navbar into component + change playlist page

--- resources/views/components/navbar.php
+++ resources/views/components/navbar.php
@@ -1,5 +1,11 @@
-<a href="<?php echo route('welcome') ?>">Home page</a>
-<a href="<?php echo route('songs') ?>">Songs page</a>
+<div class="navbar">
+    <a href="#" class="active"><i class="fas fa-home fa-2x"></i></a>
+    <a href="#"><i class="fas fa-search fa-2x"></i></a>
+    <a href="#"><i class="fas fa-cog fa-2x"></i></a>
+</div>
+
+<!-- <a href="<?php echo route('welcome') ?>">Home page</a>
+<a href="<?php echo route('songs') ?>">Songs page</a> -->
 
 <!-- <nav class="fixed left-0 top-0 right-0 w-100 flex px-5 py-3 shadow-sm" style="background: #2C0540;">
     <div class="mr-auto">MDMA</div>
--- resources/views/playlists.php
+++ resources/views/playlists.php
@@ -1,22 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MDMA - Playlists</title>
-    <link rel="stylesheet" href="css/styles.css">
-    <link rel="stylesheet" href="css/playlists.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css">
-</head>
-<body>
-    
-    <div class="content">
-        <div class="logo">MDMA</div>
-        <h1>You have no &#160;<span class="text-secondary">playlists &#160;</span> yet!</h1>
-        <a href="<?= route('login') ?>" class="btn btn-primary btn-xS">+ CREATE PLAYLIST</a>
-    </div>
-
 </body>
     <head>
         <meta charset="UTF-8">
@@ -25,46 +6,49 @@
         <title>MDMA - Playlist</title>
         <link rel="stylesheet" href="css/styles.css">
         <link rel="stylesheet" href="css/playlists.css">
+        <link href="https://kit-pro.fontawesome.com/releases/latest/css/pro.min.css" rel="stylesheet">
     </head>
     <body>
-        <div class="container">
-            <div class="search-bar">
-                <input type="text class="search-input" placeholder="Search">
-                <a class="add-playlist-btn" href="#">
-                    <img src="img/plus-2.png">
-                </a>
+        <?php view('components.navbar'); ?>
+
+        <?php if(count($playlists) == 0): ?>
+            <div class="content">
+                <div class="logo">MDMA</div>
+                <h1>You have no &#160;<span class="text-secondary">playlists &#160;</span> yet!</h1>
+                <a href="<?= route('login') ?>" class="btn btn-primary btn-xS">+ CREATE PLAYLIST</a>
             </div>
+        <?php else: ?>
+            <div class="container">
+                <div class="search-bar">
+                    <input type="text class="search-input" placeholder="Search">
+                    <a class="add-playlist-btn" href="#">
+                        <img src="img/plus-2.png">
+                    </a>
+                </div>
 
-            <h1 class="text-center">Playlists</h1>
-            <div class="splitter"></div>
+                <h1 class="text-center">Playlists</h1>
+                <div class="splitter"></div>
 
-            
-            <div class="playlist-list">
-                <?php foreach($playlists as $playlist):?>
-                    <div class="playlist">
-                        <img class="playlist-img" src="<?= $playlist->getIconUrl() ?>">
-                        <div class="playlist-details">
-                            <a class="playlist-name" href="<?= route('playlist', ['id' => $playlist->getId()]); ?>"><?= $playlist->getName() ?></a>
-                            <!-- <div class="playlist-author">Group name</div> -->
+                <div class="playlist-list">
+                    <?php foreach($playlists as $playlist):?>
+                        <div class="playlist">
+                            <img class="playlist-img" src="<?= $playlist->getIconUrl() ?>">
+                            <div class="playlist-details">
+                                <a class="playlist-name" href="<?= route('playlist', ['id' => $playlist->getId()]); ?>"><?= $playlist->getName() ?></a>
+                                <!-- <div class="playlist-author">Group name</div> -->
+                            </div>
+                            <div class="playlist-buttons">
+                                <a href="#" class="icon-btn">
+                                    <img src="img/push-pin.png">
+                                </a>
+                                <a href="#" class="icon-btn">
+                                    <img src="img/vertical-dots.png">
+                                </a>
+                            </div>
                         </div>
-                        <div class="playlist-buttons">
-                            <a href="#" class="icon-btn">
-                                <img src="img/push-pin.png">
-                            </a>
-                            <a href="#" class="icon-btn">
-                                <img src="img/vertical-dots.png">
-                            </a>
-                        </div>
-                    </div>
-                <?php endforeach ?>
+                    <?php endforeach ?>
+                </div>
             </div>
-        </div>
-        
-        <div class="navbar">
-            <a href="#" class="active"><i class="fas fa-home fa-2x"></i></a>
-            <a href="#"><i class="fas fa-search fa-2x"></i></a>
-            <a href="#"><i class="fas fa-cog fa-2x"></i></a>
-        </div>
-
+        <?php endif ?>
     </body>
 </html>
